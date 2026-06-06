98e11af209edb38a1fc6deaf194dfd47e504af0b
pumNicharee <nichar.malarat@gmail.com>
2026-06-06 21:31:00 +0700

    Update supabaseClient.js

--- eatraid/Frontend/src/app/config/supabaseClient.js
+++ eatraid/Frontend/src/app/config/supabaseClient.js
@@ -4,7 +4,8 @@ const supabaseKey = process.env.NEXT_PUBLIC_ANON_KEY
 export const supabase = createClient(supabaseUrl, supabaseKey)
 export const api_port = 3300
 export const web_port = 3000
-export const NEXT_PUBLIC_BASE_API_URL = `http://localhost:${api_port}`
+// export const NEXT_PUBLIC_BASE_API_URL = `http://localhost:${api_port}`
+export const NEXT_PUBLIC_BASE_API_URL = `https://eatraid.onrender.com`
 export const NEXT_PUBLIC_BASE_WEB_URL = `http://localhost:${web_port}`
 
  

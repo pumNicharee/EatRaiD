c9b8a80f41d3e05ed0a0b2ceb2711479b0b992c9
pumNicharee <nichar.malarat@gmail.com>
2026-06-06 21:52:13 +0700

    Update server.js

--- eatraid/Backend/server.js
+++ eatraid/Backend/server.js
@@ -11,9 +11,16 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     credentials: true,
+//   })
+// );
+
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://eat-rai-d-15pa.vercel.app",
     credentials: true,
   })
 );
